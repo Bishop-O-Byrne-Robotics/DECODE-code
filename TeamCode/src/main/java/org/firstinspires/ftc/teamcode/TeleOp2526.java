4cf3afdd73dba0abeb6b95ecdcd4b9dbc38fccb9
Catzilla016 <alexiosg555@gmail.com>
2025-09-13 22:32:34 -0600

    Create TeleOp2526.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2526.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class TeleOp2526 extends LinearOpMode {
+
+    public DcMotor FrontLeft = null;
+    public DcMotor FrontRight = null;
+    public DcMotor BackLeft = null;
+    public DcMotor BackRight = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
